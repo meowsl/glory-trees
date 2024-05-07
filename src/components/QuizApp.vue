426cc990aabefa6d8e38b5992f4a06a52b21d81e
Valter Frank <glup0st1@mail.ru>
2024-05-07 23:17:44 +0300

    feat: mobile adaptive quiz

--- src/components/QuizApp.vue
+++ src/components/QuizApp.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="quiz unit q-py-lg q-px-xl">
+  <div class="quiz unit q-pa-lg">
     <div class="row">
       <div class="col-12">
         <p class="quiz__title text-white text-uppercase">Что вы знаете о великой победе?</p>
       </div>
-      <div class="col-6 q-mt-xl">
+      <div class="col-12 col-md-6 q-mt-xl">
         <img
           class="quiz__image"
           :src="QuizImg"
         >
       </div>
-      <div class="quiz__subtitle col-6 q-mt-xl q-pl-xl text-white">
+      <div class="quiz__subtitle col-12 col-md-6 q-mt-xl q-pl-sm q-pl-md-xl text-white">
         <p class="main-text text-uppercase">Проверьте свои знания в викторине "История Победы"</p>
         <p class="body-text q-mt-lg text-body1">Вспомните ключевые события, главные сражения и основные даты,
           биографии
           полководцев,
           фильмы и песни о Великой Отечественной войне</p>
-        <div class="row justify-end items-center q-mt-lg">
+        <div class="link-block row justify-end items-center q-mt-lg">
           <p class="link-text text-body1">Присоединяйтесь к телеграм-боту и пройдите увлекательный квест</p>
           <a
             href="https://t.me/CP9m_bot"
