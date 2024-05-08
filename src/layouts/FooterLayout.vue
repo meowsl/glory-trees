b98d4e8d2f705e14d53e5a0660913a074dd8dbbd
Valter Frank <glup0st1@mail.ru>
2024-05-08 16:57:04 +0300

    feat: add new content footer

--- src/layouts/FooterLayout.vue
+++ src/layouts/FooterLayout.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="footer q-px-0 q-px-md-xl q-py-lg justify-end">
+  <div class="footer q-px-xl q-py-lg justify-end">
     <div class="row">
-      <div class="col-none col-md-7"></div>
-      <div class="col-12 col-md-5 row justify-around">
+      <div class="footer__contact col-12 col-md-7">
+        <p class="text-white">Если хотите рассказать о герое и посадить его дерево, <br>
+          свяжитесь с нами по почте <a
+            href="mailto:geroi@rsue.ru"
+            target="_blank"
+          >geroi@rsue.ru</a> </p>
+      </div>
+      <div class="footer__links col-12 col-md-5 row justify-around">
+        <a
+          href="https://www.economy.gov.ru/"
+          target="_blank"
+        >
+          <img :src="MinLogo">
+        </a>
         <a
           href="https://rsv.ru"
           target="_blank"
@@ -32,4 +44,5 @@
 import RSVLogo from "images/footer/rsv_logo.svg"
 import CPLogo from "images/footer/cp_logo.svg"
 import RSUELogo from "images/footer/rsue_logo.svg"
+import MinLogo from "images/footer/min_logo.svg"
 </script>
