df1cb17cbf65c41c849bcd1f22be3534f14903ec
Valter Frank <glup0st1@mail.ru>
2024-05-11 19:31:33 +0300

    fix: hide routes

--- server/apps/api/heroes/v1/routers.py
+++ server/apps/api/heroes/v1/routers.py
@@ -14,9 +14,9 @@ app_name = "heroes"
 urlpatterns = [
     path("list/", HeroesListAPIView.as_view(), name="list of heroes"),
     path("<int:pk>", HeroesAPIView.as_view(), name="retrieve hero"),
-    path("applications/", ApplicationListAPIView.as_view(), name="list of applications"),
-    path("applications/<int:pk>", ApplicationAPIView.as_view(), name="retrive application"),
+    # path("applications/", ApplicationListAPIView.as_view(), name="list of applications"),
+    # path("applications/<int:pk>", ApplicationAPIView.as_view(), name="retrive application"),
     path("applications/create", ApplicationCreateAPIView.as_view(), name="create application"),
-    path("archive/", ArchiveListAPIView.as_view(), name="list of archive"),
-    path("archive/<int:pk>", ArchiveAPIView.as_view(), name="retrieve archive")
+    # path("archive/", ArchiveListAPIView.as_view(), name="list of archive"),
+    # path("archive/<int:pk>", ArchiveAPIView.as_view(), name="retrieve archive")
 ]
